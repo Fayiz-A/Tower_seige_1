6956f246cdf83f648aa10db120e32d07e6347703
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2020-08-11 18:51:13 +0530

    added the all boxes

--- sketch.js
+++ sketch.js
@@ -11,16 +11,35 @@ var engine, world;
 var ground;
 var boxArray = [];
 
+var boxNumber = 1;
+var boxCorX = 750, boxCorY = 10, incrementBoxNumber = true;
+
 function setup() {
   createCanvas(1500, 700);
 
   engine = Engine.create();
   world = engine.world;
 
-  ground = new Ground(400, 400, 400, 40);
+  ground = new Ground(1000, 600, 700, 40);
 
-  for (var i = 0; i < 1; i++) {
-    boxArray.push(new Box(200, 200, 40, 40));
+  for (var row = 0; row < 9; row++) {
+    boxCorY = 10;
+    for (var column = 0; column < boxNumber; column++) {
+      boxArray.push(new Box(boxCorX, boxCorY, 60, 60));
+      boxCorY += 60;
+    }
+
+    if (boxNumber == 5) {
+      incrementBoxNumber = false;
+    }
+
+    if (incrementBoxNumber == false) {
+      boxNumber--;
+    } else {
+      boxNumber++;
+    }
+
+    boxCorX += 60;
   }
 
 }
